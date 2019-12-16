d730e32d4cb7bba2b6cbe5e320bcf7889d403d35
asadaziz95 <asadazizjs@gmail.com>
2019-12-16 16:45:31 +0500

    styling in blog display screen

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import IndexScreen from './../screens/IndexScreen.js';
-import {BlogProvider} from './../context/BlogContext';
+import {Provider} from './../context/BlogContext';
 
 const navigator = createStackNavigator(
   {
@@ -27,8 +27,8 @@ const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
 };
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,24 +1,46 @@
 import React, {useContext} from 'react';
 import {View, Text, StyleSheet, FlatList, Button} from 'react-native';
 import {Context} from '../context/BlogContext';
+import Feather from 'react-native-vector-icons/Feather';
 const IndexScreen = () => {
-  const {data, addBlogPost} = useContext(Context);
+  const {state, addBlogPost} = useContext(Context);
 
   return (
-    <View>
-      <Text>Index Screen</Text>
+    <View style={styles.mainView}>
       <Button title="Add Post " onPress={addBlogPost} />
       <FlatList
-        data={data}
+        data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({item}) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.icon} name="trash-2" />
+            </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 30,
+    borderTopWidth: 1,
+    borderColor: '#f76767',
+    backgroundColor: '#ebb8fc',
+    paddingHorizontal: 5,
+    // borderBottomWidth: 2,
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
 
 export default IndexScreen;
